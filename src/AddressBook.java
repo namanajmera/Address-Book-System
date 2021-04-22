c4a5fa4cdf1e13b061a9427a2b3d178e84d78ea9
Naman <cool.naman.ajmera@gmail.com>
2021-04-22 11:24:45 +0530

    Create a Address and Contact Class to create a Contact Person.

---
+++ src/AddressBook.java
@@ -0,0 +1,11 @@
+
+public class AddressBook {
+	public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book...!!!");
+        ContactPerson contactPerson = new ContactPerson("Naman", "Ajmera", "Chaksu", "Jaipur", "Rajasthan",
+                303901, 86926, "cool.naman.ajmera@gmail.com");
+        System.out.println(contactPerson.toString());
+    }
+}
+
+
